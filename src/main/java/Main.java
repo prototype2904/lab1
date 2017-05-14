0087e3192f5fe24c3f534b4f382a622f6fc5fff6
prototype2904 <stetskevichroman@gmail.com>
2017-05-14 19:50:43 +0300

    лаб1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,72 +1,144 @@
-import org.bytedeco.javacpp.*;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+import org.opencv.core.*;
+import org.opencv.core.Point;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.bytedeco.javacpp.opencv_core.CV_32FC1;
-import static org.bytedeco.javacpp.opencv_core.CV_64FC1;
-import static org.bytedeco.javacpp.opencv_core.CV_8UC3;
+import static org.opencv.calib3d.Calib3d.calibrateCamera;
+import static org.opencv.calib3d.Calib3d.drawChessboardCorners;
+import static org.opencv.calib3d.Calib3d.findChessboardCorners;
+import static org.opencv.core.CvType.CV_64FC1;
+import static org.opencv.core.CvType.CV_8UC3;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+import static org.opencv.imgcodecs.Imgcodecs.imwrite;
+import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
+import static org.opencv.imgproc.Imgproc.cornerSubPix;
+import static org.opencv.imgproc.Imgproc.cvtColor;
+
 
 /**
  * Created by Stetskevich Roman 2381.
  */
 public class Main {
 
+    private static final int BORDER_HEIGHT = 10;
+    private static final int BORDER_WIDTH = 14;
+    private static final double SQUARE_SIZE = 23.22;
 
     public static void main(String[] args) throws IOException {
-
-        List<opencv_core.Mat> imagePoints = new ArrayList<opencv_core.Mat>();
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Size patternSize = new Size(BORDER_WIDTH, BORDER_HEIGHT);
+        List<Mat> conersList = new ArrayList<Mat>();
+        List<Mat> objectsList = new ArrayList<Mat>();
         for( Integer i = 1; i <= 15; i++) {
-            BufferedImage image = ImageIO.read(Main.class.getResource("PNG/"+i.toString()+".png"));
-            byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+           Mat currentFrame;
+            Mat m_grayImage = new Mat();
 
-
-
-            opencv_core.Mat currentFrame = new opencv_core.Mat(0,0,CV_8UC3);
-            opencv_core.Mat m_cameraMatrix = new opencv_core.Mat(3, 3, CV_64FC1);
-
-            opencv_core.Mat m_distortionCoefficients = new opencv_core.Mat(5, 1, CV_64FC1);
-            opencv_core.Mat m_R = new opencv_core.Mat();
-            opencv_core.Mat m_T = new opencv_core.Mat();
-            opencv_core.Mat m_grayImage = new opencv_core.Mat();
-            List<List<opencv_core.Point3f>> m_realPoints;
-            Toolkit toolkit= Toolkit.getDefaultToolkit();
-            opencv_core.Size patternSize = new opencv_core.Size(7, 7);
-            BytePointer bytePointer = new BytePointer(pixels);
-            currentFrame = opencv_imgcodecs.imread(bytePointer);
-            currentFrame.put(bytePointer);
-            System.out.println(currentFrame.toString());
-            opencv_core.Mat corners = new opencv_core.Mat();
-            boolean chessboardCorners = opencv_calib3d.findChessboardCorners(currentFrame, patternSize, corners);
+            currentFrame = imread(Main.class.getResource("PNG/"+i.toString()+".png").getPath());
+            MatOfPoint2f corners = new MatOfPoint2f();
+            boolean chessboardCorners = findChessboardCorners(currentFrame, patternSize, corners);
             if(chessboardCorners){
-                opencv_core.TermCriteria termCriteria = new opencv_core.TermCriteria(opencv_core.TermCriteria.EPS | opencv_core.TermCriteria.MAX_ITER, 30, 0.1);
-                opencv_imgproc.cvtColor(currentFrame, m_grayImage, opencv_imgproc.CV_BGR2GRAY);
-                opencv_imgproc.cornerSubPix(m_grayImage, corners, new opencv_core.Size(11,11),
-                        new opencv_core.Size(-1, -1), new opencv_core.TermCriteria(opencv_core.TermCriteria.MAX_ITER, 30, 0.1));
-                opencv_calib3d.drawChessboardCorners(currentFrame, patternSize, corners, true);
-                imagePoints.add(corners);
+                TermCriteria termCriteria = new TermCriteria(TermCriteria.EPS | TermCriteria.MAX_ITER, 30, 0.1);
+                cvtColor(currentFrame, m_grayImage, 6);
+                cornerSubPix(m_grayImage, corners, new Size(11,11),
+                        new Size(-1, -1), termCriteria);
+                drawChessboardCorners(currentFrame, patternSize, corners, true);
+                writeMatToFile(i.toString(), corners);
+                conersList.add(corners);
+                ArrayList<Point3> point3List = new ArrayList<Point3>();
+                for(Point point : corners.toArray()){
+                    point3List.add(new Point3(point));
+                }
+                objectsList.add(new MatOfPoint3f(point3List.toArray(new Point3[point3List.size()])));
             }
-
-
         }
+
+        Mat intrinsicsMatrix = new Mat();
+        Mat distortionCoefficients = new Mat();
+        List<Mat> rotationVectors = new ArrayList<Mat>();
+        List<Mat> translationVectors = new ArrayList<Mat>();
+        double calibrateCamera = calibrateCamera(objectsList, conersList, patternSize, intrinsicsMatrix, distortionCoefficients, rotationVectors, translationVectors);
+
+        FileOutputStream fileOutputStream = new FileOutputStream(new File("output.txt"));
+        fileOutputStream.write("Intrinsics:\n".getBytes());
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int r = 0; r < intrinsicsMatrix.rows(); r++){
+            for(int c = 0; c < intrinsicsMatrix.cols(); c++){
+                stringBuilder.append(Arrays.toString(intrinsicsMatrix.get(r,c))).append(",");
+            }
+            if(r != intrinsicsMatrix.cols() -1)
+                stringBuilder.append("\n");
+        }
+        fileOutputStream.write(stringBuilder.toString().getBytes());
+        fileOutputStream.write("\nDistortion coefficients:\n".getBytes());
+        stringBuilder = new StringBuilder();
+        for(int r = 0; r < distortionCoefficients.rows(); r++){
+            for(int c = 0; c < distortionCoefficients.cols(); c++){
+                stringBuilder.append(Arrays.toString(distortionCoefficients.get(r,c))).append(",");
+            }
+            if(r != distortionCoefficients.cols() -1)
+                stringBuilder.append("\n");
+        }
+        fileOutputStream.write(stringBuilder.toString().getBytes());
+
+        fileOutputStream.write("\nRotation vectors:\n".getBytes());
+        stringBuilder = new StringBuilder();
+        for(Mat mat3 : rotationVectors) {
+            for (int r = 0; r < mat3.rows(); r++) {
+                for (int c = 0; c < mat3.cols(); c++) {
+                    stringBuilder.append(Arrays.toString(mat3.get(r, c))).append(",");
+                }
+                if (r != mat3.cols() - 1)
+                    stringBuilder.append("\n");
+            }
+            stringBuilder.append("\n\n");
+        }
+        fileOutputStream.write(stringBuilder.toString().getBytes());
+        fileOutputStream.write("\nTranslation vectors:\n".getBytes());
+        stringBuilder = new StringBuilder();
+        for(Mat mat4 : translationVectors) {
+            for (int r = 0; r < mat4.rows(); r++) {
+                for (int c = 0; c < mat4.cols(); c++) {
+                    stringBuilder.append(Arrays.toString(mat4.get(r, c))).append(",");
+                }
+                if (r != mat4.cols() - 1)
+                    stringBuilder.append("\n");
+            }
+            stringBuilder.append("\n\n");
+        }
+        fileOutputStream.write(stringBuilder.toString().getBytes());
+
     }
-//
-//    public static void main(String[] args) {
-//        for (Integer i = 1; i <= 15; i++) {
-//            BufferedImage image = null;
-//            try {
-//                image = ImageIO.read(Main.class.getResource("PNG/"+i.toString()+".png"));
-//            } catch (IOException e) {
-//            }
-//            byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-//
-//        }
-//
-//    }
+
+    private static void writeMatToFile(String numberFile, Mat mat){
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("chess_0"+numberFile+".txt"));
+            StringBuilder stringBuilder = new StringBuilder();
+            int i = 0;
+            for(int r = 0; r < mat.rows(); r++){
+                for(int c = 0; c < mat.cols(); c++){
+                    stringBuilder.append(i++).append(" ").append(Arrays.toString(mat.get(r,c))).append(",");
+                }
+                if(r != mat.cols() -1)
+                    stringBuilder.append("\n");
+            }
+            fileOutputStream.write(stringBuilder.toString().getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
